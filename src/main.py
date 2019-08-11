957bb6e56254aeef8460f88cf40eecb187f89f99
Keshik <keshik6@gmail.com>
2019-08-11 23:56:26 +0800

    lr fix

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def main(data_dir, results_dir, weights_dir,
         which_dataset, image_resize, image_crop_size, 
         exp_num,
         max_epochs, batch_size, samples_update_size, 
-        num_workers=4, lr=7.5e-5, weight_decay=1e-5):
+        num_workers=4, lr=1e-6, weight_decay=1e-5):
 
     # Define directories
     labels_dir = os.path.join(data_dir, which_dataset, "gt_files")
@@ -58,8 +58,8 @@ def main(data_dir, results_dir, weights_dir,
 
     # Create cuda parameters
     use_cuda = torch.cuda.is_available()
-    np.random.seed(2019)
-    torch.manual_seed(2019)
+    np.random.seed(2020)
+    torch.manual_seed(2020)
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # Create embedding network
@@ -104,4 +104,4 @@ def main(data_dir, results_dir, weights_dir,
 if __name__ == '__main__':
     main(data_dir="./data/", results_dir="./results", weights_dir="./weights",
         which_dataset="oxbuild", image_resize=460, image_crop_size=448,
-        exp_num=2, max_epochs=25, batch_size=5, samples_update_size=32)
+        exp_num=2, max_epochs=25, batch_size=5, samples_update_size=64)
